e1dc0c659d0eb95ae7dce52c456064ba8cf1a8d5
ykwy404 <thekaway404@gmail.com>
2022-12-02 21:30:58 -0300

    fix

--- src/helper/promovate.js
+++ src/helper/promovate.js
@@ -1,6 +1,7 @@
 const promovate = ({boardPeaces, x, y, p, setBoardPeaces, promovateAudio}) => {
+    console.log(p)
     //auto promovate to queen
-    if(y === 1){
+    if(y === 1 && boardPeaces[y - 1][x - 1] === 1){
         const oldBoardPeace = [...boardPeaces]
         oldBoardPeace[y - 1][x - 1] = 5
         setBoardPeaces(oldBoardPeace)
